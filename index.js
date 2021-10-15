f1e2b1d76c8c48a53d133ec8fa42bc2484067a76
wb1110 <strengthcoachwb@gmail.com>
2021-10-15 07:42:07 -0500

    topic 1 done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function myFunction() {
 myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+// "Nested function is able to access variable internal through closure. Functions are able to reach upstream outside of their scope to obtain values"
 
 
 
@@ -28,11 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(x) {
+  let counter = x;
+  for (let i = 0; i < x; i++){
+     counter += i;
   }
- 
+ return counter;
+}
+ console.log(summation(4));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
